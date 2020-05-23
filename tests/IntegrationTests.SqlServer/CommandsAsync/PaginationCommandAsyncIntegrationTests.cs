14b63442e65ccc06bb119d202ec01035be24ce46
Scott Ferguson <scott.ferguson82@gmail.com>
2020-05-22 23:25:01 -0500

    Adding async tests

---
+++ tests/IntegrationTests.SqlServer/CommandsAsync/PaginationCommandAsyncIntegrationTests.cs
@@ -0,0 +1,157 @@
+﻿using FluentCommander.Database;
+using Shouldly;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace IntegrationTests.SqlServer.CommandsAsync
+{
+    [Collection("Service Provider collection")]
+    public class PaginationCommandAsyncIntegrationTests : IntegrationTest<IDatabaseCommander>
+    {
+        public PaginationCommandAsyncIntegrationTests(ServiceProviderFixture serviceProviderFixture, ITestOutputHelper output)
+            : base(serviceProviderFixture, output) { }
+
+        [Fact]
+        public async Task ExecutePaginationAsync_ShouldReturnRowsAsExpected_WhenUsingMinimalInput()
+        {
+            // Arrange && Act
+            PaginationCommandResult result = await SUT.BuildCommand()
+                .ForPagination()
+                .From("[dbo].[SampleTable]")
+                .ExecuteAsync(new CancellationToken());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.HasData.ShouldBeTrue();
+            result.TotalCount.ShouldBeGreaterThan(0);
+
+            // Print
+            WriteLine("Total Count: {0}", result.TotalCount);
+            WriteLine(result.DataTable);
+        }
+
+        [Fact]
+        public async Task ExecutePaginationAsync_ShouldReturnRowsAsExpected_WhenSelectingColumns()
+        {
+            // Arrange && Act
+            PaginationCommandResult result = await SUT.BuildCommand()
+                .ForPagination()
+                .Select("[SampleInt]")
+                .From("[dbo].[SampleTable]")
+                .ExecuteAsync(new CancellationToken());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.HasData.ShouldBeTrue();
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.DataTable.Columns.Count.ShouldBe(1);
+
+            // Print
+            WriteLine("Total Count: {0}", result.TotalCount);
+            WriteLine(result.DataTable);
+        }
+
+        [Fact]
+        public async Task ExecutePaginationAsync_ShouldReturnRowsAsExpected_WhenFilteringRows()
+        {
+            // Arrange && Act
+            PaginationCommandResult result = await SUT.BuildCommand()
+                .ForPagination()
+                .From("[dbo].[SampleTable]")
+                .Where("[SampleTableID] = 1")
+                .ExecuteAsync(new CancellationToken());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.HasData.ShouldBeTrue();
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.DataTable.Rows.Count.ShouldBe(1);
+
+            // Print
+            WriteLine("Total Count: {0}", result.TotalCount);
+            WriteLine(result.DataTable);
+        }
+
+        [Fact]
+        public async Task ExecutePaginationAsync_ShouldReturnRowsAsExpected_WhenOrderingRows()
+        {
+            // Arrange && Act
+            PaginationCommandResult result = await SUT.BuildCommand()
+                .ForPagination()
+                .From("[dbo].[SampleTable]")
+                .OrderBy("[SampleTableID] DESC")
+                .ExecuteAsync(new CancellationToken());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.HasData.ShouldBeTrue();
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.DataTable.Rows[0]["SampleTableID"].ShouldNotBeNull();
+            Convert.ToInt64(result.DataTable.Rows[0]["SampleTableID"].ToString()).ShouldBe(ExecuteScalar<long>("SELECT MAX([SampleTableID]) FROM [dbo].[SampleTable]"));
+
+            // Print
+            WriteLine("Total Count: {0}", result.TotalCount);
+            WriteLine(result.DataTable);
+        }
+
+        [Fact]
+        public async Task ExecutePaginationAsync_ShouldReturnRowsAsExpected_WhenSettingPageSize()
+        {
+            // Arrange
+            const int pageSize = 10;
+
+            // Act
+            PaginationCommandResult result = await SUT.BuildCommand()
+                .ForPagination()
+                .From("[dbo].[SampleTable]")
+                .PageSize(pageSize)
+                .ExecuteAsync(new CancellationToken());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.HasData.ShouldBeTrue();
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.DataTable.Rows.Count.ShouldBe(pageSize);
+
+            // Print
+            WriteLine("Total Count: {0}", result.TotalCount);
+            WriteLine(result.DataTable);
+        }
+
+        [Fact]
+        public async Task ExecutePaginationAsync_ShouldReturnRowsAsExpected_WhenAllSettingsAreUsed()
+        {
+            // Arrange
+            const int pageSize = 25;
+            const int pageNumber = 2;
+
+            // Act
+            PaginationCommandResult result = await SUT.BuildCommand()
+                .ForPagination()
+                .Select("[SampleTableID]")
+                .From("[dbo].[SampleTable]")
+                .Where("[SampleTableID] < 100")
+                .OrderBy("1")
+                .PageSize(pageSize)
+                .PageNumber(pageNumber)
+                .ExecuteAsync(new CancellationToken());
+
+            // Assert
+            result.ShouldNotBeNull();
+            result.HasData.ShouldBeTrue();
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.DataTable.Columns.Count.ShouldBe(1);
+            result.DataTable.Rows.Count.ShouldBe(pageSize);
+            result.DataTable.Rows[0][0].ShouldNotBeNull();
+            Convert.ToInt32(result.DataTable.Rows[0][0]).ShouldBe(pageSize + 1);
+            Convert.ToInt32(result.DataTable.Rows[pageSize - 1][0]).ShouldBe(pageSize * pageNumber);
+
+            // Print
+            WriteLine("Total Count: {0}", result.TotalCount);
+            WriteLine(result.DataTable);
+        }
+    }
+}
