b9b31ec1b8ad680ad9d0711df0405e824a6665a3
Scott Ferguson <scott.ferguson82@gmail.com>
2020-06-20 18:39:56 -0500

    Adding support for strongly-typed OrderHints()

--- src/FluentCommander/Core/Ordering/ColumnOrderingBuilder.cs
+++ src/FluentCommander/Core/Ordering/ColumnOrderingBuilder.cs
@@ -1,4 +1,9 @@
-﻿namespace FluentCommander.Core.Ordering
+﻿using FluentCommander.Core.Property;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace FluentCommander.Core.Ordering
 {
     public class ColumnOrderingBuilder
     {
@@ -22,6 +27,49 @@
             _request.ColumnOrdering.ColumnOrders.Add(new ColumnOrder(columnName, OrderDirection.Descending));
 
             return this;
+        }
+    }
+
+    public class ColumnOrderingBuilder<TEntity>
+    {
+        private readonly IHaveColumnOrdering _request;
+
+        public ColumnOrderingBuilder(IHaveColumnOrdering request)
+        {
+            _request = request;
+        }
+
+        public ColumnOrderingBuilder<TEntity> Build(Action<PropertyMapBuilder<TEntity>> propertyMappingBuilder)
+        {
+            _request.ColumnOrdering = ParseToColumnOrdering(propertyMappingBuilder);
+
+            return this;
+        }
+
+        private ColumnOrdering ParseToColumnOrdering(Action<PropertyMapBuilder<TEntity>> propertyMappingBuilder)
+        {
+            var columnOrdering = new ColumnOrdering();
+
+            var propertyMapBuilder = new PropertyMapBuilder<TEntity>();
+
+            propertyMappingBuilder(propertyMapBuilder);
+
+            foreach (PropertyMetadata<TEntity, object> property in propertyMapBuilder.PropertyMap.PropertyMetadataCollection.Where(p => !p.IsIgnore))
+            {
+                var member = property.Selector.Body as MemberExpression;
+                var unary = property.Selector.Body as UnaryExpression;
+
+                string name = ((MemberExpression)(member ?? unary?.Operand))?.Member.Name;
+                
+                columnOrdering.ColumnOrders.Add(
+                    new ColumnOrder
+                    {
+                        ColumnName = name,
+                        Direction = property.OrderDirection
+                    });
+            }
+
+            return columnOrdering;
         }
     }
 }
